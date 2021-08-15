0ad3ea6d8f9453a6e83ccb7f3102171fb58f2137
Sargis Hakobyan <sargishakobyann97@gmail.com>
2021-08-15 10:31:40 +0400

    release 1

--- src/components/fire.js
+++ src/components/fire.js
@@ -1,13 +1,13 @@
 import firebase from "firebase"
 
 var firebaseConfig = {
-    apiKey: "AIzaSyAKDobFgejH7h3YOQ49ImmCcEZnKAfU4TE",
-    authDomain: "beeweb-c5dfa.firebaseapp.com",
-    projectId: "beeweb-c5dfa",
-    storageBucket: "beeweb-c5dfa.appspot.com",
-    messagingSenderId: "403634708359",
-    appId: "1:403634708359:web:c483e08c5d9cef600ffd22",
-    measurementId: "G-HE4GRM91Z6"
+    apiKey: "AIzaSyCO3b8QseTKoV9I36Cc18_L1P5csd3Fj5w",
+    authDomain: "test-1d751.firebaseapp.com",
+    projectId: "test-1d751",
+    storageBucket: "test-1d751.appspot.com",
+    messagingSenderId: "552451510023",
+    appId: "1:552451510023:web:cd7fd48ee4dc075ab57fa2",
+    measurementId: "G-W6M11GTQW8"
 };
 
 const fire = firebase.initializeApp(firebaseConfig);
